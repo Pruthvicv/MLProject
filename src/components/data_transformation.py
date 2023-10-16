b5435c1f05a9a2020209f88f6cff2f1ad6cc1e3f
Pruthvicv <pruthvicv77@gmail.com>
2023-10-16 12:52:01 +0530

    updates notebook

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -87,6 +87,7 @@ class DataTransformation:
             logging.info("Reading train and test data completed")
 
             logging.info("obtaining preprocessing object")
+           
 
             preprocessing_obj = self.get_data_transformer_object()
 
@@ -94,11 +95,11 @@ class DataTransformation:
 
             numerical_features = ['reading_score', 'writing_score']
 
-            input_feature_train_df = train_df.drop(columns=[target_column_name],axis=1)
-            target_feature_train_df =train_df[target_column_name] 
+            input_feature_train_df=train_df.drop(columns=["math_score"],axis=1)
+            target_feature_train_df=train_df[target_column_name]
 
-            input_feature_test_df = test_df.drop(columns=[target_column_name],axis=1)
-            target_feature_test_df =test_df[target_column_name]  
+            input_feature_test_df=test_df.drop(columns=[target_column_name],axis=1)
+            target_feature_test_df=test_df[target_column_name] 
 
 
             logging.info("applying preprocessing object on training dataframe and test dataframe")
